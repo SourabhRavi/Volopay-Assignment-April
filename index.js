2d440d23aab3e4693d03b737371322f3f9250b5e
Sourabh Kumar Ravi <66831910+SourabhRavi@users.noreply.github.com>
2021-04-10 18:02:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 
+document.addEventListener('DOMContentLoaded', function(event) {
+    document.querySelector('body').style.opacity = 1
+})
+
+// Store username and password in Session Storage of browser//
+window.sessionStorage.setItem("username", "permit");
+window.sessionStorage.setItem("password", "set@123");
+
+//check username and password//
+let form = document.getElementById("form");
+let username = document.getElementById("username");
+let password = document.getElementById("password");
+let submit = document.getElementById("submit");
+
+form.addEventListener("submit", e => {
+    e.preventDefault();
+    if (username.value === window.sessionStorage.getItem("username") && password.value === window.sessionStorage.getItem("password")) {
+        let href = "./main.html";
+        // console.log("matched!");
+        loadPage(href);
+    } else if (username.value === "" && password.value === "") {
+        alert("Please enter Username and Password.");
+    } else if (username.value === "") {
+        alert("Username required.");
+    } else if (password.value === "") {
+        alert("Password required.");
+    } else {
+        alert("Incorrect Username/Password");
+        username.value = "";
+        password.value = "";
+    }
+});
+
+
+
+let loadPage = (link) => {
+    document.querySelector('body').style.opacity = 0;
+    setTimeout(() => {
+        window.location.href = link;
+    }, 500)
+}
